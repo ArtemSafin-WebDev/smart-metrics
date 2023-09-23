d4f26672ec6ea50dded0a8804570a1c2d491428a
Artem Safin <artemsafin.webdev@gmail.com>
2023-09-23 20:15:58 +0300

    Правки

--- pages-data/newsArchive.js
+++ pages-data/newsArchive.js
@@ -5,42 +5,42 @@ const newsArchive = {
       {
         title:
           "Сотрудники торговых точек стали в 2 раза чаще воровать деньги из кассы",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/1.webp",
         source: "/images/news-labels/1.webp",
       },
       {
         title:
           "Производители РТ нацелились на региональный ритейл и магазины «у дома»",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/2.webp",
         source: "/images/news-labels/2.webp",
       },
       {
         title:
           "Производители РТ нацелились на региональный ритейл и магазины «у дома»",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/2.webp",
         source: "/images/news-labels/2.webp",
       },
       {
         title:
           "90% продавцов не рассказывают покупателям об акциях — исследование",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/1.webp",
         source: "/images/news-labels/3.webp",
       },
       {
         title:
           "Сотрудники торговых точек стали в 2 раза чаще воровать деньги из кассы",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/1.webp",
         source: "/images/news-labels/1.webp",
       },
       {
         title:
           "Производители РТ нацелились на региональный ритейл и магазины «у дома»",
-        date: "16 января",
+        date: "16.01.2023",
         src: "/images/news/2.webp",
         source: "/images/news-labels/2.webp",
       },
